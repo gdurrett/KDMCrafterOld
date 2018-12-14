c5e402cd18310c5ae10c500bf85d71a02fbc4556
Greg Durrett <gdurrett@gregs-imac.local>
2018-12-14 11:27:34 -0800

    Initial Commit

---
+++ KDMCrafter/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  KDMCrafter
+//
+//  Created by Greg Durrett on 12/14/18.
+//  Copyright © 2018 AppHazard Productions. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
